770327fd2f619f7a3997f2cc7a020dd97dd8d271
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2021-06-05 22:59:22 +0500

    advance mern autherntication & protected routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('colors');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const connectDB = require('./config/db');
 
@@ -17,6 +18,15 @@ app.get('/', (req, res, next) => {
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/private', require('./routes/private'));
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () =>
